1c8b707d006bdb254fbda21c8ae19047e65be7b0
Vlad Matyushin <particular0010abyss@gmail.com>
2024-10-04 22:02:00 -0700

    Realized Connection Client/Server And .pdf Guarantee

--- VocalizeItServer/app.py
+++ VocalizeItServer/app.py
@@ -1,8 +1,16 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, File, UploadFile
 import os
-from pdfminer.high_level import extract_text
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 UPLOAD_FOLDER = 'uploads/'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
@@ -13,17 +21,10 @@ async def upload_file(file: UploadFile = File(...)):
         file_location = f"{UPLOAD_FOLDER}/{file.filename}"
         with open(file_location, "wb+") as file_object:
             file_object.write(file.file.read())
-
-        extracted_text = extract_text(file_location)
-        text_filename = f"{UPLOAD_FOLDER}/{file.filename.rsplit('.', 1)[0]}.txt"
-
-        with open(text_filename, 'w', encoding='utf-8') as text_file:
-            text_file.write(extracted_text)
-
-        return {"message": "File processed", "txt_file": text_filename}
+        return {"message": "Jija Jidkaya!"}
     else:
         return {"error": "Only PDF files are allowed"}
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8001)
